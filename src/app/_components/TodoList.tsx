00f5e0d4dffebfed2336a28bbce148cbd56d2db8
Jake Pitman <jakeseanp@gmail.com>
2024-11-09 17:37:14 +0900

    Add 'addTodo' mutation

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -4,10 +4,18 @@ import { trpc } from "../_trpc/client";
 
 export default function TodoList() {
   const getTodos = trpc.todo.getTodos.useQuery();
+  const addTodo = trpc.todo.addTodo.useMutation().mutate;
 
   return (
     <div>
       <div>{JSON.stringify(getTodos.data)}</div>
+      <button
+        onClick={() => {
+          addTodo({ title: "New TODO" });
+        }}
+      >
+        Add TODO
+      </button>
     </div>
   );
 }
--- src/server/routers/todo.ts
+++ src/server/routers/todo.ts
@@ -1,7 +1,16 @@
 import { publicProcedure, router } from "../trpc";
+import { z } from "zod";
+
+const TODOS = ["TODO 1", "TODO 2", "TODO 3"];
 
 export const todo = router({
   getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30];
+    return TODOS;
   }),
+  addTodo: publicProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(async (opts) => {
+      TODOS.push(opts.input.title);
+      return TODOS;
+    }),
 });
